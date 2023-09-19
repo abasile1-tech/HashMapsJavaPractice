a1f84ae507efab41c39090ac9c3c72c5ac558bd8
John <ashton.basile@gmail.com>
2023-09-19 13:53:09 +0100

    some hashmap practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, String> favoriteFruits;
+        favoriteFruits = new HashMap<>();
+
+        favoriteFruits.put("Jason", "Banana");
+        favoriteFruits.put("Alice", "Apple");
+        favoriteFruits.put("Melinda", "Melon");
+        favoriteFruits.put("Sarah", "Strawberry");
+
+        System.out.println(favoriteFruits.get("Alice"));
+        System.out.println(favoriteFruits.containsKey("Harry"));
+        System.out.println(favoriteFruits.containsValue("Melon"));
+
+        favoriteFruits.remove("Sarah");
+        System.out.println(favoriteFruits);
+
+        favoriteFruits.clear();
+        System.out.println(favoriteFruits);
     }
 }
